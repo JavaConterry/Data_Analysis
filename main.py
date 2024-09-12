78e1c907865732402b4d2b4b8daf3556c0fa7c0f
JavaConterry <conterry05@gmail.com>
2024-09-12 17:54:16 +0200

    dataset change

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from urllib.request import urlopen
 import pandas as pd
+import matplotlib.pyplot as plt
 
-response = urlopen('https://api.openf1.org/v1/weather?csv=true&meeting_key=1237')
-data = pd.read_csv(response)
-data.to_csv('canadagp_2024_weather.csv')
+df = pd.read_csv('heart.csv')
+
+print(df.info())
+print(df.describe())
+
+int_cols = ['Age', 'RestingBP', 'Cholesterol', 'FastingBS', 'MaxHR', 'Oldpeak', 'HeartDisease']
+df = df.drop(df.loc[df['Cholesterol'] == 0].index)
+df = df.reset_index(drop=True)
+
+plt.plot(df[int_cols])
+plt.legend(int_cols)
+plt.show()
+
+print(df.info())
+print(df.describe())
+
+df.to_csv('heart_preprocessed.csv')
