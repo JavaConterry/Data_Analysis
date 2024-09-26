4b39f09506d3a984624950213e79cf7cef4d79b0
JavaConterry <conterry05@gmail.com>
2024-09-26 15:33:34 +0200

    preprocessing

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ df = pd.read_csv('diabetes_012_health_indicators_BRFSS2015.csv')
 print(df.info())
 print(df.describe())
 
-print(np.corrcoef(df.sample(10000)))
+print('Pearson coef:\n', np.corrcoef(df.sample(10000)))
+input()
