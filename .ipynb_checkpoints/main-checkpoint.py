e4d6cc462ff22a2568f2ed0a9875fd92c9ceefaf
JavaConterry <conterry05@gmail.com>
2024-12-01 18:09:02 +0100

    lin alg

--- .ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv('diabetes_012_health_indicators_BRFSS2015.csv')
-print(df.info())
-print(df.describe())
-
-print('Pearson coef:\n', np.corrcoef(df.sample(10000)))
-input()
